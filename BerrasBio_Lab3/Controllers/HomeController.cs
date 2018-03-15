77618ee7014ba6e14c5b7421121d2fc908522b7c
Andekl <andreas.eklundit94@gmail.com>
2018-03-15 13:31:17 +0100

    Started on lab

--- BerrasBio_Lab3/Controllers/HomeController.cs
+++ BerrasBio_Lab3/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BerrasBio_Lab3.Models;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace BerrasBio_Lab3.Controllers
 {
@@ -12,7 +14,16 @@ namespace BerrasBio_Lab3.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+        DataTable dt = new DataTable();
+        string connection_string = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BerrasBio;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection con = new SqlConnection(connection_string))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Filmer", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+                return View(dt);
         }
 
         public IActionResult About()
